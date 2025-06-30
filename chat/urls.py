1eafbf214d960de6238d47f43339a19568237410
Manish Rakaskoti <helloworld00ff@gmail.com>
2025-06-30 16:04:31 +0545

    feat: Chat feature test

--- chat/urls.py
+++ chat/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('test/', views.test, name='test'),
+    path('login/', TemplateView.as_view(template_name='chat/login.html'), name='login'),
 ]
