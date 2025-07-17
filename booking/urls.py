425e419b158d813cf806c8cb2d8a04a9d322796a
Manish Rakaskoti <helloworld00ff@gmail.com>
2025-07-17 16:24:06 +0545

    updated url

--- booking/urls.py
+++ booking/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path, include
+from .views import RentBookingViewSet
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register(r'rent-booking', RentBookingViewSet, basename='rent-booking')
 
 urlpatterns = [
-    # path('rent-booking/', include('booking.urls')),
+    path('booking/', include(router.urls)),
 ]
