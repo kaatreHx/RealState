0acd2bfd5fef75e226d7c43f650eae933cb04b80
Manish Rakaskoti <helloworld00ff@gmail.com>
2025-07-18 14:17:33 +0545

    Booking Foreign key takes id instead of object

--- booking/serializers.py
+++ booking/serializers.py
@@ -1,14 +1,19 @@
 from rest_framework import serializers
 from .models import RentBooking
-from users.serializers import CustomUserSerializer
-from property.serializers import PropertySerializer
+from users.models import CustomUser
+from property.models import Property
 
 class RentBookingSerializer(serializers.ModelSerializer):
 
-    user = CustomUserSerializer()
-    property = PropertySerializer()
+    user = serializers.PrimaryKeyRelatedField(read_only=True)
+    property = serializers.PrimaryKeyRelatedField(queryset=Property.objects.all())
 
     class Meta:
         model = RentBooking
         fields = '__all__'
+        read_only_fields = ('status', 'user')
+    
+    def create(self, validated_data):
+        user = self.context['request'].user
+        return RentBooking.objects.create(user=user, **validated_data)
 
