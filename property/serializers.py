e07c3d504db0f4fb5a488b9e082bcf0f4fa194ea
Manish Rakaskoti <helloworld00ff@gmail.com>
2025-07-18 14:18:33 +0545

    Serializer changes

--- property/serializers.py
+++ property/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import Property, PropertyImage, PropertyCart, PropertyRating
+from users.models import CustomUser, CustomerVerification
 
 class PropertyImageSerializer(serializers.ModelSerializer):
     class Meta:
@@ -7,11 +8,12 @@ class PropertyImageSerializer(serializers.ModelSerializer):
         fields = ['image']
 
 class PropertySerializer(serializers.ModelSerializer):
+    owner = serializers.PrimaryKeyRelatedField(read_only=True)
     images = PropertyImageSerializer(many=True, read_only=True)
     class Meta:
         model = Property
         fields = '__all__'
-        read_only_fields = ['created_at']
+        read_only_fields = ['owner', 'created_at', 'updated_at']
 
     def validate(self, data):
         if data['price'] < 0:
@@ -27,8 +29,19 @@ class PropertySerializer(serializers.ModelSerializer):
         if data['area'] < 0:
             raise serializers.ValidationError("Area cannot be negative")
         return data
+    
+    def create(self, validated_data):
+        user = self.context['request'].user
+        info = CustomUser.objects.filter(id = user.id).first()
+        check = info.is_owner
+        check_verification = info.is_verified
+        if not check:
+            raise serializers.ValidationError("Not permission to create property")
+        if not check_verification:
+            raise serializers.ValidationError("Not verified")
         
-
+        return Property.objects.create(owner=user, **validated_data)
+    
 class PropertyCartSerializer(serializers.ModelSerializer):
     class Meta:
         model = PropertyCart
