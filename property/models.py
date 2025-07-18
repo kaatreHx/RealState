3a2111a3b9353aabb89fe4d23cb90b916ca621a1
Manish Rakaskoti <helloworld00ff@gmail.com>
2025-07-18 14:18:14 +0545

    Property nmodel changes

--- property/models.py
+++ property/models.py
@@ -31,17 +31,17 @@ class Property(models.Model):
     city = models.CharField(max_length=100)
     state = models.CharField(max_length=100)
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    bedrooms = models.IntegerField()
-    bathrooms = models.IntegerField()
-    kitchen = models.IntegerField()
-    parking = models.IntegerField()
-    area = models.IntegerField()
+    bedrooms = models.IntegerField(null=True, blank=True)
+    bathrooms = models.IntegerField(null=True, blank=True)
+    kitchen = models.IntegerField(null=True, blank=True)
+    parking = models.IntegerField(null=True, blank=True)
+    area = models.IntegerField(null=True, blank=True)
     description = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return f"Owner: {self.owner.first_name} {self.owner.last_name} - Property Type: {self.property_type} - Address: {self.address}"
+        return f"Owner: {self.owner} - Property Type: {self.property_type} - Address: {self.address}"
 
 def upload_property_images(instance, filename):
     return f"property_images/{instance.property_id}/{filename}"
